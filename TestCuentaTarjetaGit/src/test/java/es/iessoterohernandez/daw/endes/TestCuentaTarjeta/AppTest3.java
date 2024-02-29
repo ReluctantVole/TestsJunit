de0f1b6828f5b4efdb780d248165f8581c1f99ef
ReluctantVole <150682494+ReluctantVole@users.noreply.github.com>
2024-02-29 12:41:51 +0100

    Add files via upload

---
+++ TestCuentaTarjetaGit/src/test/java/es/iessoterohernandez/daw/endes/TestCuentaTarjeta/AppTest3.java
@@ -0,0 +1,20 @@
+package es.iessoterohernandez.daw.endes.TestCuentaTarjeta;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest3 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ TestCuentaTarjetaGit/src/test/java/es/iessoterohernandez/daw/endes/TestCuentaTarjeta/AppTest4.java
@@ -0,0 +1,20 @@
+package es.iessoterohernandez.daw.endes.TestCuentaTarjeta;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest4 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
